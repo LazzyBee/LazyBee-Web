5f2fb0fc842ab6ef573cf3801a4c3ae340a4c9f5
Tran An <antp@itpro.vn>
2015-07-01 16:41:04 +0700

    building framework

---
+++ src/main/java/com/born2go/lazzybee/client/LazzyBee.java
@@ -0,0 +1,13 @@
+package com.born2go.lazzybee.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class LazzyBee implements EntryPoint{
+
+	@Override
+	public void onModuleLoad() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
